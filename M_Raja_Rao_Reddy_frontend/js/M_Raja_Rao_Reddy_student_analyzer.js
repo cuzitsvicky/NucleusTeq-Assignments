69cc94d2e49937b396a4dda35e7109fb49711d67
cuzitsvicky <en22cs301558@medicaps.ac.in>
2026-03-19 21:38:59 +0530

    feat: add student data

---
+++ M_Raja_Rao_Reddy_frontend/js/M_Raja_Rao_Reddy_student_analyzer.js
@@ -0,0 +1,36 @@
+const students = [
+  {
+    name: "Lalit",
+    marks: [
+      { subject: "Math", score: 78 },
+      { subject: "English", score: 82 },
+      { subject: "Science", score: 74 },
+      { subject: "History", score: 69 },
+      { subject: "Computer", score: 88 }
+    ],
+    attendance: 82
+  },
+  {
+    name: "Rahul",
+    marks: [
+      { subject: "Math", score: 90 },
+      { subject: "English", score: 85 },
+      { subject: "Science", score: 80 },
+      { subject: "History", score: 76 },
+      { subject: "Computer", score: 92 }
+    ],
+    attendance: 91
+  },
+  {
+    name: "Priya",
+    marks: [
+      { subject: "Math", score: 88 },
+      { subject: "English", score: 90 },
+      { subject: "Science", score: 85 },
+      { subject: "History", score: 88 },
+      { subject: "Computer", score: 87 }
+    ],
+    attendance: 95
+  }
+];
+console.log(students);
---
+++ M_Raja_Rao_Reddy_frontend/js/README.md
@@ -0,0 +1,11 @@
+## 📊 Console Output Screenshots & Explanations
+ 
+### Screenshot 1: Add multiple students
+
+#### Input :-
+
+![alt text](<Screenshot 2026-03-19 212015.png>)
+
+#### Output :-
+
+![alt text](<Screenshot 2026-03-19 212137.png>)
