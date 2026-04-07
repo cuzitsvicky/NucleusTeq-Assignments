911077cc5eddca23f8c33ef9ae3004011347cf67
cuzitsvicky <en22cs301558@medicaps.ac.in>
2026-04-07 15:08:58 +0530

    Add Area Calculator and Even/Odd Checker

--- MRajaRaoReddy_java_training/session1/Basics/AreaCalculator.java
+++ MRajaRaoReddy_java_training/session1/Basics/AreaCalculator.java
@@ -1,5 +1,39 @@
 package MRajaRaoReddy_java_training.session1.Basics;
 
+import java.util.Scanner;
+
 public class AreaCalculator {
-    
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Digit for Area Calculation: ");
+        System.out.println("1. Circle");
+        System.out.println("2. Rectangle");
+        System.out.println("3. Triangle");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.print("Enter the radius of the circle: ");
+                double radius = scanner.nextDouble();
+                System.out.println("Area of the circle: " + (Math.PI * radius * radius));
+                break;
+            case 2:
+                System.out.print("Enter the length of the rectangle: ");
+                double length = scanner.nextDouble();
+                System.out.print("Enter the width of the rectangle: ");
+                double width = scanner.nextDouble();
+                System.out.println("Area of the rectangle: " + (length * width));
+                break;
+            case 3:
+                System.out.print("Enter the base of the triangle: ");
+                double base = scanner.nextDouble();
+                System.out.print("Enter the height of the triangle: ");
+                double height = scanner.nextDouble();
+                System.out.println("Area of the triangle: " + (0.5 * base * height));
+                break;
+            default:
+                System.out.println("Please select a valid option");
+        }
+        scanner.close();
+    }
 }
---
+++ MRajaRaoReddy_java_training/session1/Basics/EvenOdd.java
@@ -0,0 +1,18 @@
+package MRajaRaoReddy_java_training.session1.Basics;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String args[]) {
+        // Take input from the user
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number to check if it's even or odd: ");
+        int number = input.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Number is even");
+        } else {
+            System.out.println("Number is odd");
+        }
+    }
+}
