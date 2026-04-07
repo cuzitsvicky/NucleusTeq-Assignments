adb304974573951f91509f7fe232e3d8afc79992
cuzitsvicky <en22cs301558@medicaps.ac.in>
2026-04-07 18:26:50 +0530

    Add Multiplication Table Program

---
+++ MRajaRaoReddy_java_training/session1/ControlFlowStatements/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package MRajaRaoReddy_java_training.session1.ControlFlowStatements;
+
+import java.util.Scanner;
+
+//3) Use a for loop to print a multiplication table.
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+        scanner.close();
+    }
+}
