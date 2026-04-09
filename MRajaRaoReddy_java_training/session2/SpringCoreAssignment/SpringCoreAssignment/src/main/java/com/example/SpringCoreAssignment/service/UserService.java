629d122822c9bbc1921821df0512b359b5402fe6
cuzitsvicky <en22cs301558@medicaps.ac.in>
2026-04-10 00:05:40 +0530

    Implement UserService with CRUD operations for User management

---
+++ MRajaRaoReddy_java_training/session2/SpringCoreAssignment/SpringCoreAssignment/src/main/java/com/example/SpringCoreAssignment/service/UserService.java
@@ -0,0 +1,53 @@
+package com.example.SpringCoreAssignment.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.SpringCoreAssignment.model.User;
+import com.example.SpringCoreAssignment.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    private  final UserRepository userRepository;
+
+    // Constructor Injection
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(int id) {
+        return userRepository.findById(id);
+    }
+
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User updateUser(int id, User user) {
+        User existingUser = userRepository.findById(id);
+
+        if (existingUser == null) {
+            throw new RuntimeException("User not found with id: " + id);
+        }
+
+        return userRepository.update(id, user);
+    }
+
+    public String deleteUser(int id) {
+
+        boolean deleted = userRepository.deleteUser(id);
+
+        if (!deleted) {
+            throw new RuntimeException("User not found with id: " + id);
+        }
+
+        return "User deleted successfully";
+    }
+}
