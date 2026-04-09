cce25a9489cf3e7b0919cb84a749e1177c41fd6b
cuzitsvicky <en22cs301558@medicaps.ac.in>
2026-04-09 23:50:03 +0530

    Initial Spring Boot project setup

---
+++ MRajaRaoReddy_java_training/session2/SpringCoreAssignment/SpringCoreAssignment/src/main/java/com/example/SpringCoreAssignment/SpringCoreAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringCoreAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreAssignmentApplication.class, args);
+	}
+
+}
---
+++ MRajaRaoReddy_java_training/session2/SpringCoreAssignment/SpringCoreAssignment/src/test/java/com/example/SpringCoreAssignment/SpringCoreAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringCoreAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
