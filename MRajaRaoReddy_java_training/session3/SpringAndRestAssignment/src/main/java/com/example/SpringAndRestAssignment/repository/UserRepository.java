0ba223bc534aca96688de08f83da5ae5b88564a6
cuzitsvicky <en22cs301558@medicaps.ac.in>
2026-04-14 22:49:28 +0530

    feat: Implement UserRepository for user data management with CRUD operations

---
+++ MRajaRaoReddy_java_training/session3/SpringAndRestAssignment/src/main/java/com/example/SpringAndRestAssignment/repository/UserRepository.java
@@ -0,0 +1,41 @@
+package com.example.SpringAndRestAssignment.repository;
+
+
+
+import java.util.*;
+import org.springframework.stereotype.Repository;
+
+import com.example.SpringAndRestAssignment.model.User;
+
+@Repository
+public class UserRepository {
+
+    private final List<User> users = new ArrayList<>();
+
+    public UserRepository() {
+        users.add(new User(1L, "Priya", 25, "USER"));
+        users.add(new User(2L, "Rahul", 30, "ADMIN"));
+        users.add(new User(3L, "Amit", 30, "USER"));
+        users.add(new User(4L, "Sneha", 28, "USER"));
+        users.add(new User(5L, "Vikas", 35, "MANAGER"));
+        users.add(new User(6L, "Neha", 30, "USER"));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public Optional<User> findById(Long id) {
+        return users.stream()
+                .filter(u -> u.getId().equals(id))
+                .findFirst();
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void delete(User user) {
+        users.remove(user);
+    }
+}
