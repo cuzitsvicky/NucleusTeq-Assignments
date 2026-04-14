bbb1101512af7c8490dcfc57940f91daefa2acf1
cuzitsvicky <en22cs301558@medicaps.ac.in>
2026-04-14 22:31:43 +0530

    docs: Initialize Spring Boot project and basic application structure

---
+++ MRajaRaoReddy_java_training/session3/SpringAndRestAssignment/src/main/java/com/example/SpringAndRestAssignment/SpringAndRestAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringAndRestAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndRestAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndRestAssignmentApplication.class, args);
+	}
+
+}
---
+++ MRajaRaoReddy_java_training/session3/SpringAndRestAssignment/src/test/java/com/example/SpringAndRestAssignment/SpringAndRestAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringAndRestAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndRestAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
