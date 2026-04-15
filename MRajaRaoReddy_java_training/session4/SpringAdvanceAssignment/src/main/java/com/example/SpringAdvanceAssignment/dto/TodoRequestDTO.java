c4015e1d9ad359a5e6819c351b4117d4f23ed3c0
cuzitsvicky <en22cs301558@medicaps.ac.in>
2026-04-15 20:40:58 +0530

    feat: Add TodoRequestDTO and TodoResponseDTO for task management

---
+++ MRajaRaoReddy_java_training/session4/SpringAdvanceAssignment/src/main/java/com/example/SpringAdvanceAssignment/dto/TodoRequestDTO.java
@@ -0,0 +1,42 @@
+package com.example.SpringAdvanceAssignment.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class TodoRequestDTO {
+
+    @NotNull(message = "Title cannot be null")
+    @Size(min = 3, message = "Title must be at least 3 characters")
+    private String title;
+
+    @NotNull(message = "Description cannot be null")
+    private String description;
+
+    @NotNull(message = "Status cannot be null")
+    private String status;
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ MRajaRaoReddy_java_training/session4/SpringAdvanceAssignment/src/main/java/com/example/SpringAdvanceAssignment/dto/TodoResponseDTO.java
@@ -0,0 +1,54 @@
+package com.example.SpringAdvanceAssignment.dto;
+import java.time.LocalDateTime;
+
+public class TodoResponseDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private LocalDateTime createdAt;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
