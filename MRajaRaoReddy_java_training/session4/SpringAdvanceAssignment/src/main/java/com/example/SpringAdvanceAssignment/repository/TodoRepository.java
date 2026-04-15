1d175bbdc051fcb18512f9ccc11013fc51f5280f
cuzitsvicky <en22cs301558@medicaps.ac.in>
2026-04-15 21:24:34 +0530

    feat: Add comment to TodoRepository interface for clarity on CRUD operations

--- MRajaRaoReddy_java_training/session4/SpringAdvanceAssignment/src/main/java/com/example/SpringAdvanceAssignment/repository/TodoRepository.java
+++ MRajaRaoReddy_java_training/session4/SpringAdvanceAssignment/src/main/java/com/example/SpringAdvanceAssignment/repository/TodoRepository.java
@@ -2,5 +2,6 @@ package com.example.SpringAdvanceAssignment.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.SpringAdvanceAssignment.entity.Todo;
 
+// This interface extends JpaRepository, providing CRUD operations for Todo entities
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
