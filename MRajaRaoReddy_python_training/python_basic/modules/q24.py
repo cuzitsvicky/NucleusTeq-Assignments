14e67b5a7b81ce5e31673f0bbe727eef922600fa
cuzitsvicky <en22cs301558@medicaps.ac.in>
2026-06-12 15:49:32 +0530

    refactor: encapsulate user input and operations in use_custom_module function

--- MRajaRaoReddy_python_training/python_basic/modules/q24.py
+++ MRajaRaoReddy_python_training/python_basic/modules/q24.py
@@ -1,15 +1,16 @@
 # Question 24: Create your own module and import it.
 
-# Importing functions from the custom module created in the same directory (custom_module.py)
 from custom_module import add_numbers, multiply_numbers, subtract_numbers, divide_numbers, power_numbers
 
-# Taking input from the user for two numbers
-first_number = int(input("Enter first number: "))
-second_number = int(input("Enter second number: "))
+def use_custom_module() -> None:
+    first_number = int(input("Enter first number: "))
+    second_number = int(input("Enter second number: "))
 
-# Using the imported functions to perform operations and print results
-print("Sum =", add_numbers(first_number, second_number))
-print("Product =", multiply_numbers(first_number, second_number))
-print("Difference =", subtract_numbers(first_number, second_number))
-print("Quotient =", divide_numbers(first_number, second_number))
-print("Power =", power_numbers(first_number, second_number))
+    print("Sum =", add_numbers(first_number, second_number))
+    print("Product =", multiply_numbers(first_number, second_number))
+    print("Difference =", subtract_numbers(first_number, second_number))
+    print("Quotient =", divide_numbers(first_number, second_number))
+    print("Power =", power_numbers(first_number, second_number))
+
+if __name__ == "__main__":
+    use_custom_module()
